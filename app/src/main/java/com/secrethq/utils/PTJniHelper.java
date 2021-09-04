445d6e9de55559b355e5e5a4b54bc0d013c0db88
aoeminh <aoeminh@gmail.com>
2021-09-04 13:20:36 +0700

    init

---
+++ app/src/main/java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,10 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "Gy37IZym4FAZKPxznvy8XEp5qSPIprUKFnr8Is+s51pIf64mmvq8D00r+CDKq+MLSi2rcM/+sVkbK/FwlarjXw==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+}
